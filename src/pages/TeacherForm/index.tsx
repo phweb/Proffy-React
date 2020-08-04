b8bcfc90ea1b89c8626c02fe9ac5e13a824e7d13
phweb <phweb53@gmail.com>
2020-08-03 22:16:13 -0300

    Criação da landing page

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import PageHeader from "../../components/PageHeader";
+
+export default function TeacherForm() {
+  return (
+    <div id="page-landing">
+       <PageHeader  title="Que incrível que você quer da aula."/>
+    </div>
+  );
+}
